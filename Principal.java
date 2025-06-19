74464c48e00d36042f6d740dc3c252d68a2be776
Rene Zazueta <rene_zaid@live.com.mx>
2025-06-19 08:11:49 -0700

    First Commit Fork

--- Principal.java
+++ Principal.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 
 public class Principal {
     public static void main(String[] args) throws IOException {
+        System.out.println("Hola");
         String codigo = "var1 int ; var2 int ; var1 := var2 + var1 ; print var1 + var2 ;  ";
         new Parser(codigo);
     }
